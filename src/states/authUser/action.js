d0d102fd5762828e6545370a9b2e56b36e79a12e
rintosaputro <saputrorinto@gmail.com>
2023-05-04 16:07:16 +0700

    feat: authUser action

--- src/states/authUser/action.js
+++ src/states/authUser/action.js
@@ -1,3 +1,45 @@
-/**
- * @TODO: Define all the actions (creator) for the authUser state
- */
+import api from '../../utils/api';
+
+const ActionType = {
+  SET_AUTH_USER: 'SET_AUTH_USER',
+  UNSET_AUTH_USER: 'SET_AUTH_USER',
+};
+
+const setAuthUserActionCreator = (authUser) => ({
+  type: ActionType.SET_AUTH_USER,
+  payload: {
+    authUser,
+  },
+});
+
+const unsetAuthUserActionCreator = () => ({
+  type: ActionType.UNSET_AUTH_USER,
+  payload: {
+    authUser: null,
+  },
+});
+
+const asyncSetAuthUser = ({ id, password }) => async (dispatch) => {
+  try {
+    const token = await api.login({ id, password });
+    api.putAccessToken(token);
+    const authUser = await api.getOwnProfile();
+
+    dispatch(setAuthUserActionCreator(authUser));
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+const asyncUnsetAuthUser = () => (dispatch) => {
+  dispatch(unsetAuthUserActionCreator());
+  api.putAccessToken('');
+};
+
+export {
+  ActionType,
+  setAuthUserActionCreator,
+  unsetAuthUserActionCreator,
+  asyncSetAuthUser,
+  asyncUnsetAuthUser,
+};
