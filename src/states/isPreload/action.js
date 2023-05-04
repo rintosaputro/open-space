0c338e99b443db3121643aef26d8067156f91a73
rintosaputro <saputrorinto@gmail.com>
2023-05-04 16:22:45 +0700

    feat: preload action

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,28 @@
-/**
- * @TODO: Define all the actions (creator) for the isPreLoad state
- */
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+const setIsPreloadActionCreator = (isPreload) => ({
+  type: ActionType.SET_IS_PRELOAD,
+  payload: { isPreload },
+});
+
+const asyncPreloadProcess = () => async (dispatch) => {
+  try {
+    const authUser = await api.getOwnProfile();
+    dispatch(setAuthUserActionCreator(authUser));
+  } catch (err) {
+    dispatch(setAuthUserActionCreator(null));
+  } finally {
+    dispatch(setIsPreloadActionCreator(false));
+  }
+};
+
+export {
+  ActionType,
+  setIsPreloadActionCreator,
+  asyncPreloadProcess,
+};
