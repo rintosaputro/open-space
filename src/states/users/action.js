1e5278388bce441b1e4d311969c051c8925759c3
rintosaputro <saputrorinto@gmail.com>
2023-05-05 04:27:29 +0700

    feat: users action

--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,3 +1,24 @@
-/**
- * @TODO: Define all the actions (creator) for the users state
- */
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_USERS: 'RECEIVE_USERS',
+};
+
+const receiveUsers = (users) => ({
+  type: ActionType.RECEIVE_USERS,
+  payload: { users },
+});
+
+const asyncRegisterUser = ({ id, name, password }) => async () => {
+  try {
+    await api.register({ id, name, password });
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export {
+  ActionType,
+  receiveUsers,
+  asyncRegisterUser,
+};
