203cc2499dcdbdd10ccae66e66de2b436d65c2c1
rintosaputro <saputrorinto@gmail.com>
2023-05-05 16:16:22 +0700

    feat: action shared

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,17 @@
-/**
- * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
- */
+import api from '../../utils/api';
+import { receiveTalksActionCreator } from '../talks/action';
+import { receiveUsers } from '../users/action';
+
+const asyncPopulateUsersAndTalks = () => async (dispatch) => {
+  try {
+    const users = await api.getAllUsers();
+    const talks = await api.getAllTalks();
+
+    dispatch(receiveUsers(users));
+    dispatch(receiveTalksActionCreator(talks));
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export { asyncPopulateUsersAndTalks };
