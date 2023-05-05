ff4f817e0f1cb8bc93dce7efa860c4a47105c1a8
rintosaputro <saputrorinto@gmail.com>
2023-05-05 13:35:54 +0700

    feat: action talks

--- src/states/talks/action.js
+++ src/states/talks/action.js
@@ -1,3 +1,59 @@
-/**
- * @TODO: Define all the actions (creator) for the talks state
- */
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_TALKS: 'RECEIVE_TALKS',
+  ADD_TALK: 'ADD_TALK',
+  TOGGLE_LIKE_TALK: 'TOGGLE_LIKE_TALK',
+};
+
+const receiveTalksActionCreator = (talks) => ({
+  type: ActionType.RECEIVE_TALKS,
+  payload: {
+    talks,
+  },
+});
+
+const addTalkActionCreator = (talk) => ({
+  type: ActionType.ADD_TALK,
+  payload: {
+    talk,
+  },
+});
+
+const toggleLikeTalkActionCreator = ({ talkId, userId }) => ({
+  type: ActionType.TOGGLE_LIKE_TALK,
+  payload: {
+    talkId, userId,
+  },
+});
+
+const asyncAddTalk = ({ text, replyTo = '' }) => async (dispatch) => {
+  try {
+    const talk = await api.createTalk({ text, replyTo });
+    dispatch(addTalkActionCreator(talk));
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+const asyncToggleLikeTalk = (talkId) => async (dispatch, getState) => {
+  // Line 42 & 43 is for better user experience using "optimistically apply action"
+  const { authUser } = getState();
+  dispatch(toggleLikeTalkActionCreator({ talkId, userId: authUser.id }));
+
+  try {
+    await api.toggleLikeTalk(talkId);
+  } catch (err) {
+    alert(err.message);
+    dispatch(toggleLikeTalkActionCreator({ talkId, userId: authUser.id }));
+  }
+};
+
+export {
+  ActionType,
+  receiveTalksActionCreator,
+  addTalkActionCreator,
+  toggleLikeTalkActionCreator,
+  asyncAddTalk,
+  asyncToggleLikeTalk,
+};
