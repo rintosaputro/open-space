601bd96d70e9ce7536c53e0775169b0d1b0cddad
rintosaputro <saputrorinto@gmail.com>
2023-05-09 14:52:43 +0700

    chore: install & configure react redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './states';
 import App from './App';
 
 import './styles/style.css';
@@ -9,9 +11,11 @@ const root = createRoot(document.getElementById('root'));
 
 // TODO: wrap App with store provider
 root.render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>,
 );
