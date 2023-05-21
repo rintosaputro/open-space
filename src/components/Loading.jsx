c597a39c447acab49b7f7a0db0b2f88292e8d95d
rintosaputro <saputrorinto@gmail.com>
2023-05-21 21:38:36 +0700

    fix: loading bar issue

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { LoadingBar } from 'react-redux-loading-bar';
+import LoadingBar from 'react-redux-loading-bar';
 
 function Loading() {
   return (
-    <div className="loading">
-      <LoadingBar />
-    </div>
+    <LoadingBar className="loading" />
   );
 }
 
